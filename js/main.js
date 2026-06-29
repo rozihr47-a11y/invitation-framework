0a78c431ee780578ad06824a196c42c43bf480c0
rozihr47-a11y <rozi.hr47@gmail.com>
2026-06-29 22:18:50 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
-console.log("Loaded from externaltest haha!");
+document.addEventListener("DOMContentLoaded", () => {
 
-document.body.style.outline = "5px solid lime";
+    Invitation.init();
+
+});
